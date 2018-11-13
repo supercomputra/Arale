0156756e25e8fa078eb681323c5b97db5dd2bd0c
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2018-11-13 14:41:04 +0700

    Initial Commit

---
+++ Arale/Arale.h
@@ -0,0 +1,19 @@
+//
+//  Arale.h
+//  Arale
+//
+//  Created by Zulwiyoza Putra on 13/11/18.
+//  Copyright © 2018 Zulwiyoza Putra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Arale.
+FOUNDATION_EXPORT double AraleVersionNumber;
+
+//! Project version string for Arale.
+FOUNDATION_EXPORT const unsigned char AraleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Arale/PublicHeader.h>
+
+
